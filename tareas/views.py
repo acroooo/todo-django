821f39bd2f67dcfbda4772a211d0da78ddb29253
Hernan Chamorro <hernang.work@gmail.com>
2021-07-14 17:51:27 -0300

    actualizarTarea view + url ¿

--- tareas/views.py
+++ tareas/views.py
@@ -4,21 +4,32 @@ from django.shortcuts import redirect, render
 from .forms import *
 from .models import *
 
-
 # Create your views here.
-def index(request):
+
+# Vista de tareas (lista) y creacion de tarea.
+def index(req):
     tareas = Tarea.objects.all()
     
     form = TareaForm()
     
     # request
-    if request.method == 'POST':
-        form = TareaForm(request.POST)
+    if req.method == 'POST':
+        form = TareaForm(req.POST)
         if form.is_valid():
             form.save()
         return redirect('/')
+    
+    # contexto
     contexto = {
         'tareas': tareas, 
         'form': form
     }
-    return render(request, 'tareas/lista.html', contexto)
+    
+    # render
+    return render(req, 'tareas/lista.html', contexto)
+
+
+# Actualizar tarea
+def ActualizarTarea(req, id):
+    tarea = Tarea.objects.get(id=id)
+    return render(req, 'tareas/actualizar_tarea.html')
