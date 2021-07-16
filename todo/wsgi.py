dc814f4b9743b5d615d637271f27748414363c0a
Hernan Chamorro <hernang.work@gmail.com>
2021-07-16 20:13:26 -0300

    fix wsgi

--- todo/wsgi.py
+++ todo/wsgi.py
@@ -12,7 +12,5 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todo.settings')
-from dj_static import Cling
-from django.core.wsgi import get_wsgi_application
 
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
