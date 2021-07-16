71f83688bce32bf0cad0c17cd018709a4fb20fff
Hernan Chamorro <hernang.work@gmail.com>
2021-07-16 19:49:54 -0300

    changes wsgi and procfile

--- todo/wsgi.py
+++ todo/wsgi.py
@@ -12,5 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todo.settings.dev')
+from dj_static import Cling
+from django.core.wsgi import get_wsgi_application
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
